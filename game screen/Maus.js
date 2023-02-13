66e3807176643294367f4fce748c31ed32179e33
Schmokes <lesa22kz@student.ju.se>
2023-02-13 17:48:04 +0100

    Maus

---
+++ game screen/Maus.js
@@ -0,0 +1,52 @@
+background(0, 150, 255);
+/* 
+translate(200,200); */
+
+//HEAD
+
+//Ear fill  color
+
+//Ear left
+push();
+beginShape();
+vertex(225, 125);
+vertex(230, 95);
+vertex(250, 90);
+vertex(260, 100);
+vertex(259, 107);
+vertex(240, 130);
+endShape();
+pop();
+
+//Ear right
+push();
+beginShape();
+vertex(235, 130);
+vertex(250, 106);
+vertex(270, 107);
+vertex(272, 122);
+vertex(263, 133);
+vertex(244, 137);
+endShape();
+pop();
+
+//Wiskers top
+
+//Head
+push();
+beginShape();
+vertex(160, 165);
+vertex(162, 190);
+vertex(242, 195);
+bezierVertex(250, 130, 240, 110, 195, 115);
+bezierVertex(170, 120, 150, 120, 130, 120);
+bezierVertex(120, 120, 120, 135, 133, 147);
+bezierVertex(157, 165, 180, 178, 190, 173);
+endShape();
+pop();
+
+//Nose
+
+//Eye left
+
+//Eye right
